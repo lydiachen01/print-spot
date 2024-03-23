a593d20c296772a63b6e91fd054bbe25d01ab5d4
lydiachen01 <137934459+lydiachen01@users.noreply.github.com>
2024-03-23 17:14:39 -0400

    Added TailwindCSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import MapComponent from '../components/map';
 
-const IndexPage: React.FC = () => {
+const Homepage: React.FC = () => {
     return (
         <div>
-            <h1>Printer Map</h1>
+            <h1 className='bg-slate-500 text-white center'>Printer Map</h1>
             <MapComponent />
         </div>
     );
 };
 
-export default IndexPage;
+export default Homepage;
